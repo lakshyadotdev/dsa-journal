3a012183f4456967c5cb85cb6227fff75bb74384
lakshyadotdev <real.lakshyapanchal@gmail.com>
2026-08-06 18:59:28 +0530

    Added Solution - GfG to GitHub

---
+++ gfg/Difficulty: Basic/The Else if Statement/the-else-if-statement.cpp
@@ -0,0 +1,11 @@
+void utility(int number) {
+    if(number >100){
+        cout << "Big\n";
+    }
+    else if(number < 10){
+        cout << "Small\n";
+    }
+    else{
+        cout << "Number\n";
+    }
+}
