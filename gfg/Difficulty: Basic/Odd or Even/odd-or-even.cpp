d6c68787dc3275e36e18f7f55bb91057ab7f17f8
lakshyadotdev <real.lakshyapanchal@gmail.com>
2026-08-10 10:57:04 +0530

    Updated Solution - GfG to GitHub

--- gfg/Difficulty: Basic/Odd or Even/odd-or-even.cpp
+++ gfg/Difficulty: Basic/Odd or Even/odd-or-even.cpp
@@ -1,6 +1,11 @@
 class Solution {
   public:
     bool isEven(int n) {
-        return n%2==0;
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };
