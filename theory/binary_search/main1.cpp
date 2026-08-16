b790de06367b530b25157407fde6c858cabdc7a8
lakshyadotdev <real.lakshyapanchal@gmail.com>
2026-08-16 12:51:40 +0530

    feat: add recursive binary search function and update main to demonstrate usage

--- theory/binary_search/main1.cpp
+++ theory/binary_search/main1.cpp
@@ -25,11 +25,33 @@ int binarySearch(vector<int> &arr, int &target)
     return -1;
 }
 
+int binarySearchRec(vector<int> &arr, int target, int start, int end)
+{
+    if (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (target > arr[mid])
+        {
+            return binarySearchRec(arr, target, mid + 1, end);
+        }
+        else if (target < arr[mid])
+        {
+            return binarySearchRec(arr, target, start, mid - 1);
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    return -1;
+}
+
 int main(int argc, char const *argv[])
 {
     vector<int> v1 = {-1, 0, 3, 4, 5, 9, 12, 134};
     int tar1 = 12;
     cout << binarySearch(v1, tar1) << endl;
+    cout << binarySearchRec(v1, tar1, 0, v1.size() - 1) << endl;
     for (int i = 0; i < v1.size(); i++)
     {
         cout << v1[i] << " ";
