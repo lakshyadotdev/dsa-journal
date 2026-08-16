1e2c5081f320072484531ad3fb02f3d85dc6554a
lakshyadotdev <real.lakshyapanchal@gmail.com>
2026-08-16 12:39:47 +0530

    feat: refactor binary search implementation into a separate function

--- theory/binary_search/main1.cpp
+++ theory/binary_search/main1.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int> &arr, int &target)
+{
+    int start = 0;
+    int end = arr.size() - 1;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        if (target > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else if (target < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    return -1;
+}
 
 int main(int argc, char const *argv[])
 {
-
+    vector<int> v1 = {-1, 0, 3, 4, 5, 9, 12, 134};
+    int tar1 = 12;
+    cout << binarySearch(v1, tar1) << endl;
+    for (int i = 0; i < v1.size(); i++)
+    {
+        cout << v1[i] << " ";
+    }
     return 0;
 }
