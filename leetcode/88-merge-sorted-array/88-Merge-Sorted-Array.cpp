484dc3926cef7e5a7c627f0f162be101698e902e
lakshyadotdev <real.lakshyapanchal@gmail.com>
2026-08-24 15:02:14 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 42.80%] - LeetPush

--- leetcode/88-merge-sorted-array/88-Merge-Sorted-Array.cpp
+++ leetcode/88-merge-sorted-array/88-Merge-Sorted-Array.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=m; i<m+n; i++){
-            nums1[i] = nums2[i-m];
+        int i1 = m - 1;
+        int i2 = n - 1;
+        int k = m + n - 1;
+        while (i2>=0) {
+            if (i1 >= 0 && nums1[i1] > nums2[i2]) {
+                nums1[k] = nums1[i1];
+                i1--;
+            }
+            else{
+                nums1[k] = nums2[i2];
+                i2--;
+            }
+            k--;
         }
-        sort(nums1.begin(),nums1.end());
     }
 };
